8ea6dd523677515a817d9dee287099359f07ccd3
VictorCaminero <caminerotorres@gmail.com>
2020-01-11 18:11:40 -0400

    Initial commit on Jan 11th

---
+++ src/DefaultPackage/Calculator.java
@@ -0,0 +1,58 @@
+package DefaultPackage;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class Calculator extends MethodsClass {
+
+    public static void main(String[] args) throws IOException {
+
+        printMsg("Enter the number 1: ");
+        String str = readValue();
+        while (!isNumeric(str)) {
+            printMsg("This value is not numeric, enter a valid number: ");
+            str = readValue();
+        }
+        BigDecimal num1 = str2Decimal(str);
+
+        printMsg("Enter the number 2: ");
+        String str2 = readValue();
+        while (!isNumeric(str2)) {
+            printMsg("This value is not numeric, enter a valid number: ");
+            str2 = readValue();
+        }
+        BigDecimal num2 = str2Decimal(str2);
+
+        printMsg("Enter desired Math operation: ");
+        String operation = readValue();
+
+        while (!isValidOp(operation)) {
+            printMsg("This operation is invalid, enter a valid operation: ");
+            operation = readValue();
+        }
+
+        switch (operation) {
+            case "+":
+                BigDecimal add = sum2Num(num1, num2);
+                printMsg("The result of the sum is: " + add);
+                break;
+            case "-":
+                BigDecimal rest = rest2Num(num1, num2);
+                printMsg("The result of the subtraction is: " + rest);
+                break;
+            case "*":
+                BigDecimal mult = mult2Num(num1, num2);
+                printMsg("The result of multiplication es: " + mult);
+                break;
+            case "/":
+                BigDecimal division = divide2Num(num1, num2);
+                if (division==null)
+                    printMsg("Sorry, the result of this operation is not valid, try again from scratch");
+                 else
+                    printMsg("The result of division is: " + division);
+                break;
+        }
+
+    } // this is the end of main method
+
+}
---
+++ src/DefaultPackage/MethodsClass.java
@@ -0,0 +1,70 @@
+package DefaultPackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class MethodsClass {
+
+    public static void printMsg(String str) {
+        System.out.println(str);
+    }
+
+    public static String readValue() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return reader.readLine();
+    }
+
+    public static boolean isNumeric(String str) {
+        try {
+            BigDecimal bigDecimal = new BigDecimal(str).setScale(2, BigDecimal.ROUND_DOWN);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static BigDecimal str2Decimal(String num) {
+        BigDecimal bigDecimal = new BigDecimal(num).setScale(2, BigDecimal.ROUND_DOWN);
+        return bigDecimal;
+    }
+
+    public static boolean isValidOp(String str) {
+        int valid = 0;
+        String [] value = {"+","-","*","/"};
+        for (String s : value) {
+            if (!s.equals(str)) {
+                valid++;
+            }
+        }
+        return valid==value.length-1;
+    }
+
+    public static BigDecimal sum2Num(BigDecimal a, BigDecimal b) {
+        BigDecimal result = new BigDecimal(String.valueOf(a)).add(b);
+        return result;
+    }
+
+    public static BigDecimal rest2Num(BigDecimal a, BigDecimal b) {
+        BigDecimal result = new BigDecimal(String.valueOf(a)).subtract(b);
+        return result;
+    }
+
+    public static BigDecimal mult2Num(BigDecimal a, BigDecimal b){
+        BigDecimal result = new BigDecimal(String.valueOf(a)).multiply(b);
+        return result;
+    }
+
+    public static BigDecimal divide2Num(BigDecimal a, BigDecimal b){
+        try {
+            BigDecimal result = new BigDecimal(String.valueOf(a)).divide(b, 2, RoundingMode.HALF_UP);
+            return result;
+        } catch (ArithmeticException e) {
+            System.out.println("Division by zero is not possible");
+            return null;
+        }
+    }
+
+}
